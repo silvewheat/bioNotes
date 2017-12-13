aac87f9a50ad6518dc8ca37ef813aa6d668cf487
silvewheat <silverwheat@163.com>
2017-12-13 17:30:36 +0800

    update python script

--- 02_readsMapping/produce_bwa_samse_sampe.py
+++ 02_readsMapping/produce_bwa_samse_sampe.py
@@ -9,7 +9,7 @@ import os
 import click
 
 
-def produce_samse(ref, infastq, insai, bwa, samtools, outcram, rgid, lb, pl, smid):
+def produce_samse(ref, infastq, insai, bwa, samtools, outfile, rgid, lb, pl, smid, outfmt):
     return f"""{bwa} \\
     samse \\
     -r '@RG\\tID:{rgid}\\tLB:{lb}\\tPL:{pl}\\tSM:{smid}' \\
@@ -23,19 +23,14 @@ def produce_samse(ref, infastq, insai, bwa, samtools, outcram, rgid, lb, pl, smi
 {samtools} \\
     sort \\
     --reference {ref} \\
-    -o {outcram} \\
+    --output-fmt {outfmt} \\
+    -o {outfile} \\
     - \\
     && \\
-echo "samse is done" \\
-    && \\
-{samtools} \\
-    index \\
-    {outcram} \\
-    && \\
-echo "index is done"
+echo "samse is done"
     """
 
-def produce_sampe(ref, in1fastq, in2fastq, in1sai, in2sai, bwa, samtools, outcram, rgid, lb, pl, smid):
+def produce_sampe(ref, in1fastq, in2fastq, in1sai, in2sai, bwa, samtools, outfile, rgid, lb, pl, smid, outfmt):
     return f"""{bwa} \\
     sampe \\
     -r '@RG\\tID:{rgid}\\tLB:{lb}\\tPL:{pl}\\tSM:{smid}' \\
@@ -51,16 +46,11 @@ def produce_sampe(ref, in1fastq, in2fastq, in1sai, in2sai, bwa, samtools, outcra
 {samtools} \\
     sort \\
     --reference {ref} \\
-    -o {outcram} \\
+    --output-fmt {outfmt} \\
+    -o {outfile} \\
     - \\
     && \\
-echo "sampe is done" \\
-    && \\
-{samtools} \\
-    index \\
-    {outcram} \\
-    && \\
-echo "index is done"
+echo "sampe is done"
     """
 
 
@@ -86,22 +76,23 @@ def split_sepe(fastqs):
 
 @click.command()
 @click.option('--ref', help='参考基因组的路径')
-@click.option('--outcramdir', help='输出cram结果的文件')
+@click.option('--outdir', help='输出比对结果的文件')
 @click.option('--insaidir', help='sai文件所在的文件夹')
 @click.option('--bwa', help='BWA的路径', default='bwa')
 @click.option('--samtools', help='samtools的路径', default='samtools')
+@click.option('--outfmt', help='输出文件的格式,BAM或CRAM,默认BAM', default='BAM')
 @click.option('--pl', help='测序平台, default is ILLUMINA', default='ILLUMINA')
 @click.argument('infastqs', nargs=-1)
-def main(ref, outcramdir, insaidir, bwa, samtools, pl, infastqs):
+def main(ref, outdir, insaidir, bwa, samtools, outfmt, pl, infastqs):
     sefiles, pefiles = split_sepe(infastqs)
     for nfile, infastq in enumerate(sefiles, 1):
         basename = os.path.basename(infastq).split('.')[0]
         insai = os.path.join(insaidir, basename + '.sai')
-        outcram = os.path.join(outcramdir, f'{basename}.cram')
+        outfile = os.path.join(outdir, f'{basename}.{outfmt.lower()}')
         rgid = basename
         lb = basename
         smid = basename.split('_')[0]
-        cmd = produce_samse(ref, infastq, insai, bwa, samtools, outcram, rgid, lb, pl, smid)
+        cmd = produce_samse(ref, infastq, insai, bwa, samtools, outfile, rgid, lb, pl, smid, outfmt)
         with open(f'bwa_samse_{nfile}.sh', 'w') as f:
             f.write(cmd)
     for npair, (in1fastq, in2fastq) in enumerate(zip(pefiles[::2], pefiles[1::2]), 1):
@@ -109,11 +100,11 @@ def main(ref, outcramdir, insaidir, bwa, samtools, pl, infastqs):
         basename2 = os.path.basename(in2fastq).split('.')[0]
         in1sai = os.path.join(insaidir, basename1 + '.sai')
         in2sai = os.path.join(insaidir, basename2 + '.sai')
-        outcram = os.path.join(outcramdir, f'{basename1[:-2]}.cram')
+        outfile = os.path.join(outdir, f'{basename1[:-2]}.{outfmt.lower()}')
         rgid = basename1[:-2]
         lb = basename1[:-2]
         smid = basename1.split('_')[0]
-        cmd = produce_sampe(ref, in1fastq, in2fastq, in1sai, in2sai, bwa, samtools, outcram, rgid, lb, pl, smid)
+        cmd = produce_sampe(ref, in1fastq, in2fastq, in1sai, in2sai, bwa, samtools, outfile, rgid, lb, pl, smid, outfmt)
         with open(f'bwa_sampe_{npair}.sh', 'w') as f:
             f.write(cmd)
 

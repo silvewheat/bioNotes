ec3b191b6317dda599237e7f88c3bc559d941957
silvewheat <silverwheat@163.com>
2017-12-21 09:53:06 +0800

    upgrade about mem set

--- 02_readsMapping/produce_gatk_realn.py
+++ 02_readsMapping/produce_gatk_realn.py
@@ -12,7 +12,7 @@ import click
 
 def produce_RTC(ref, gatk, mem, inbam, outinterval, tmpdir, outcram, nt):
     return f"""java -Djava.io.tmpdir={tmpdir} \\
-    -Xmx{mem}g \\
+    -Xmx{mem * nt}g \\
     -jar {gatk} \\
     -T RealignerTargetCreator \\
     -R {ref} \\
@@ -42,7 +42,7 @@ def produce_IR(ref, gatk, mem, inbam, outinterval, tmpdir, outcram):
 @click.option('--outdir', help='输出局部重比后的cram文件路径')
 @click.option('--gatk', help='gatk的路径')
 @click.option('--tmpdir', help='输出临时文件的目录')
-@click.option('--mem', help='运行gatk的内存，单位是GB')
+@click.option('--mem', type=float, help='单线程的java启动内存，单位GB，默认4', default=4)
 @click.option('--nt', help='运行RTC时的线程数')
 @click.argument('inbams', nargs=-1)
 def main(ref, outdir, gatk, tmpdir, mem, nt, inbams):

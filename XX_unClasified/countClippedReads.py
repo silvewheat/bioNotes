4dcffc2738c58e1b24acf40ff9ef07e6d5f3f525
silvewheat <silverwheat@163.com>
2020-03-05 20:38:26 +0800

    Update countClippedReads.py
    little change

--- XX_unClasified/countClippedReads.py
+++ XX_unClasified/countClippedReads.py
@@ -27,13 +27,15 @@ def load_alnfile(alnfile, reffile):
 
 def get_clip_site(record, cigarstring):
     """
-    如果存在多个clip位点，只报告处于最右边或者最左边的clip位点（右>左）
+    如果存在多个clip位点，只报告处于最右边或者最
+
+    左边的clip位点（右>左）
     """
     cigarstring = cigarstring.strip('0123456789')
     blocks = record.get_blocks()
     if (cigarstring[-1] == 'S') or (cigarstring[-1] == 'H'):
-        edge_left = blocks[-1][-1] # covered
-        edge_right = edge_left + 1 # uncovered
+        edge_left = blocks[-1][-1] # covered site
+        edge_right = edge_left + 1 # uncovered site
         return 'rightclip', edge_left, edge_right
     elif (cigarstring[0] == 'S') or (cigarstring[0] == 'H'):
         edge_right = blocks[0][0] # covered
@@ -60,18 +62,22 @@ def main(alnfile, reffile, region, outfile):
     """
     result = []
     smid, aln = load_alnfile(alnfile, reffile)
+    start, end = [int(x) for x in region.split(':')[1].split('-')]
     for record in aln.fetch(region=region):
         cigarstring = record.cigarstring
         count = Counter(cigarstring)
         n_soft = count.get('S', 0)
         n_hard = count.get('H', 0)
+        name = record.query_name
+        region_overlap = record.get_overlap(start, end)
         if n_soft + n_hard == 0:
-            continue
+            clip_type = None
+            edge_left = None
+            edge_right = None
         else:
             clip_type, edge_left, edge_right = get_clip_site(record, cigarstring)
-            name = record.query_name
-            result.append([smid, name, clip_type, edge_left, edge_right])
-    df = pd.DataFrame(result, columns=['sample_id', 'read_name', 'clip_type', 'edge_left', 'edge_right'])
+        result.append([smid, name, region_overlap, clip_type, edge_left, edge_right])
+    df = pd.DataFrame(result, columns=['sample_id', 'read_name', 'region_overlap', 'clip_type', 'edge_left', 'edge_right'])
     df.to_csv(outfile, sep='\t', index=False)
 
 
